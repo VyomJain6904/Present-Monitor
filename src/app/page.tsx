019bea25a0c6adf9b99bb57d5ed7dc46f6ed6185
VyomOp2 <vyom.malo6904@gmail.com>
2025-02-24 10:47:22 +0530

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import { GlobeDemo } from "@/components/globe_dashboard";
 import { InfiniteMovingCardsDemo } from "@/components/infinite-cards";
-import { LinkPreviewDemo } from "@/components/link-preview";
 import { AnimatedTooltipPreview } from "@/components/tooltip";
 
 
@@ -10,7 +9,6 @@ export default function Home() {
 	  return (
 		    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
 			      <GlobeDemo />
-				  {/* <LinkPreviewDemo /> */}
 				  <InfiniteMovingCardsDemo />
 				  <AnimatedTooltipPreview />
 		    </main>
